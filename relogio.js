d9d6518eddea5252302feebeb97f625d8b2a9dfe
erikmfa <erikmfa190230@gmail.com>
2022-06-15 12:12:52 -0300

    botão definir alarme funcionando

--- relogio.js
+++ relogio.js
@@ -21,37 +21,59 @@ function Alarme(){
     var alarme = document.getElementById("divAlarme");
 
     //cria os selects com os horários
-    var hrs = document.createElement("select");
-    for(var i = 0; i < 25; i++){
-        var horasOpt = document.createElement("option");
-        horasOpt.value = i;
-        horasOpt.innerHTML = i;
-        hrs.appendChild(horasOpt);
-    }
-
-    var mins = document.createElement("select");
-    for(var i = 0; i < 25; i++){
-        var minutosOpt = document.createElement("option");
-        minutosOpt.value = i;
-        minutosOpt.innerHTML = i;
-        mins.appendChild(minutosOpt);
-    }
-
     var secs = document.createElement("select");
-    for(var i = 0; i < 25; i++){
+    secs.setAttribute("id", "segundoSelect") 
+    for(var i = 0; i <= 59; i++){
         var segundosOpt = document.createElement("option");
         segundosOpt.value = i;
         segundosOpt.innerHTML = i;
         secs.appendChild(segundosOpt);
     }
 
-    var defineAlarme = document.createElement("button");
-    defineAlarme.textContent = "Definir"
+    var mins = document.createElement("select");
+    mins.setAttribute("id", "minutoSelect")
+    for(var i = 0; i <= 59; i++){
+        var minutosOpt = document.createElement("option");
+        minutosOpt.value = i;
+        minutosOpt.innerHTML = i;
+        mins.appendChild(minutosOpt);
+    }
+
+    var hrs = document.createElement("select");
+    hrs.setAttribute("id", "horaSelect")
+    for(var i = 0; i <= 23; i++){
+        var horasOpt = document.createElement("option");
+        horasOpt.value = i;
+        horasOpt.innerHTML = i;
+        hrs.appendChild(horasOpt);
+    }
+
+    let defineAlarm = document.createElement("button");
+    defineAlarm.textContent = "Definir"
+    defineAlarm.name = "definir"
+    defineAlarm.setAttribute("id", "define")
 
     alarme.appendChild(hrs);
     alarme.appendChild(mins);
     alarme.appendChild(secs);
-    alarme.appendChild(defineAlarme);
+    alarme.appendChild(defineAlarm);
 
+    defineAlarm.addEventListener("click", function(){
+        Define()
+    })
+    
+}
 
+function Define(){
+    horaAlarme = document.getElementById("horaSelect").value
+    minutoAlarme = document.getElementById("minutoSelect").value
+    segundoAlarme = document.getElementById("segundoSelect").value
+
+    var paragrafo = document.createElement("p")
+    var div = document.createElement("div")
+    paragrafo.textContent = "Alarme definido para: " + horaAlarme + " : " + minutoAlarme + " : " + segundoAlarme
+    div.appendChild(paragrafo)
+    divAlarme.appendChild(div)
+    
+    console.log(horaAlarme, minutoAlarme, segundoAlarme)
 }
