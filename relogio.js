5ae2316d1c30ebf94b8bee3efbd8e27e435946dd
erikmfa <erikmfa190230@gmail.com>
2022-06-23 02:22:37 -0300

    consultando API de alarmes

--- relogio.js
+++ relogio.js
@@ -2,17 +2,34 @@ var divRelogio = document.getElementById("divRelogio");
 var divAlarme = document.getElementById("divAlarme");
 var divCronometro = document.getElementById("divCronometro");
 var chamaAlarme = document.getElementById("botaoDoAlarme");
-var refresh = setInterval(Relogio,500);
+var mostraAlarmes = document.getElementById("botaoMostraAlarmes")
+var refresh = setInterval(relogio,500);
 var alarmeDiv = 0;
-Relogio();
+var requestURL = "https://erik-mfa.github.io/alarmes.json/alarmes.json"
+var request = new XMLHttpRequest();
+var response;
+relogio();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    response = request.response;
+    console.log(response);
+}
+
+mostraAlarmes.addEventListener("click", function(){
+    mostra()
+}, {once : true})
 
 chamaAlarme.addEventListener("click", function(){
-    Alarme()
+    alarme()
 }, {once : true})
 
 //função do relógio
 //é executada a todo o momento
-function Relogio(){
+function relogio(){
     var data = new Date();
     var horas = data.getHours();
     var minutos = data.getMinutes();
@@ -23,8 +40,12 @@ function Relogio(){
 
 }
 
+function mostra(){
+    
+}
+
 //função para criar menu do alarme
-function Alarme(){
+function alarme(){
     //cria os selects com os horários
     var secsSelect = document.createElement("select");
     secsSelect.setAttribute("id", "segundoSelect")
@@ -71,45 +92,45 @@ function Alarme(){
     //chama função do alarme
     defineAlarm.addEventListener("click", function(){
         ++alarmeDiv
-        Define(alarmeDiv)
+        define(alarmeDiv)
     })
 }
 
 //função para definir o alarme
-function Define(alarmeDiv){
+function define(alarmeDiv){
     var horaAlarme = document.getElementById("horaSelect").value
     var minutoAlarme = document.getElementById("minutoSelect").value
     var segundoAlarme = document.getElementById("segundoSelect").value //string <-----
     
     var paragrafo = document.createElement("p")
     var div = document.createElement("div")
-    paragrafo.textContent = "Alarme: " + horaAlarme + " : " + minutoAlarme + " : " + segundoAlarme 
+    paragrafo.textContent = "Alarme: " + horaAlarme + " : " + minutoAlarme
     div.appendChild(paragrafo)
     div.setAttribute("id", alarmeDiv)
     divAlarme.appendChild(div) 
-    //se o alarme DIV alarme diminuir deleta a DIV
 
     //envia uma vez o numero para ser comparado com a hora atual
     var alarme = horaAlarme + minutoAlarme + segundoAlarme
-    Envia(alarme, alarmeDiv)
+    
+    envia(alarme, alarmeDiv)
 }
 
 //variavel que executa a função do alarme
 var enviaAlarme 
 
-function Envia(alarme, alarmeDiv){
+function envia(alarme, alarmeDiv){
     
     if(alarme){
         enviaAlarme = setInterval(valor, 1000)
     }
 
     function valor(){
-        Comparar(alarme, alarmeDiv)
+        comparar(alarme, alarmeDiv)
     }
 }
 
 
-function Comparar(alarme, alarmeDiv){
+function comparar(alarme, alarmeDiv){
     var data = new Date();
     var horas = data.getHours();
     var minutos = data.getMinutes();
@@ -128,17 +149,19 @@ function Comparar(alarme, alarmeDiv){
 
         if(alarmes[0] == tempo){
             var musica = document.getElementById("myAudio")
+            var div = document.getElementById(alarmeDiv)
+ 
             alarmes.splice(0, alarmes.length)
             clearInterval(enviaAlarme)
             enviaAlarme = undefined
-            var div = document.getElementById(alarmeDiv)
+            
             div.remove();
 
             function tocar(){
                 musica.play()
             }
-            tocar();
 
+            tocar();
         }
 
     }
